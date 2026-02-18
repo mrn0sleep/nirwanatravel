b342c4ee15e8bd20de16c323165a6316ebce5a08
Bombomf12 <teofilusaditya194@gmail.com>
2026-02-18 10:54:46 -0500

    still fixing bug

--- login/login.php
+++ login/login.php
@@ -1,3 +1,26 @@
+<?php
+
+session_start();
+
+$erros = [
+    'login' => $_SESSION['login_error'] ??'', 
+    'register' => $_SESSION[''] ??''
+];
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error){
+    return !empty($error) ? "<p class='erorr-massage'> $error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm) {
+    return $formName === $activeForm ? 'active' : '';
+}
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,9 +32,10 @@
   <body>
 
     <div class="container">
-        <div class="form-box active" id="login-form">
+        <div class="form-box <?= isActiveForm ('login', $activeForm); ?>" id="login-form">
             <form action="login_register.php" method="post">
                 <h2>Login</h2>
+                <?= "showError"($errors['login']); ?>
                 <input type="email" name="email" maxlength="80" placeholder="EMAIL" required>
                 <input type="password" name="password" placeholder="PASSWORD" required>
                 <button type="submit" name="login">Login</button>   
@@ -19,13 +43,14 @@
             </form>
         </div>
 
-        <div class="form-box" id="register-form">
+        <div class="form-box <?= isActiveForm ('register', $activeForm); ?>" id="register-form">
             <form action="login_register.php" method="post">
                 <h2>Registrasi</h2>
+                <?= "showError"($errors['register']); ?>
                 <input type="text" name="name" placeholder="NAMA" required>
                 <input type="email" name="email" placeholder="EMAIL" required>
                 <input type="password" name="password" placeholder="PASSWORD" required>
-                <button type="submit" name="login">Registrasi</button>
+                <button type="submit" name="register">Registrasi</button>
                 <p>Sudah mempunyai akun? <a href="#" onclick="showForm('login-form')">Login</a></p>
             </form>
         </div>
